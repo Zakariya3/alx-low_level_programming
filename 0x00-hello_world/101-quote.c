83641e95dd4644fbe8811f851c3902bfac1fee9e
zakariya3 <zakariaelgamh3@gmail.com>
2023-03-18 15:36:42 -0700

    task 0000

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
+ *
  * Return 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
-return (1);
+	write(2,
+	"and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	return (1);
 }	
+
