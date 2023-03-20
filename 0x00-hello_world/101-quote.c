47969c70ea1164b70b4324d19562e1019ab4be53
zakariya3 <zakariaelgamh3@gmail.com>
2023-03-20 13:49:10 -0700

    Z&H

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,22 @@
-#include <stdio.h> 
-#include <stdlib.h>
+/*
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <unistd.h>
 
 /**
- * main - Entry point
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
  *
- * Return 1 (Success)
+ * Return: Always 1.
  */
 int main(void)
 {
 	write(2,
-	"and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
 	return (1);
-}	
+}
 
