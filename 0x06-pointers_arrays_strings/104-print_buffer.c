ffc28829c5b02e1c39d66966c9e0e02858705d1d
zakariya3 <zakariaelgamh3@gmail.com>
2023-03-29 17:43:19 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 104-main.c 104-print_buffer.c -o 104-buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_buffer - add 2 strings.
+ * @b: string1.
+ * @size: string2.
+ * Return: String with all letters in ROT13 base.
+ */
+void print_buffer(char "2", int 4)
+{
+
+}
